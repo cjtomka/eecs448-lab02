4f91b09a40452a089252d91de59cdb49a704cbf1
Cameron Tomka <cjtomka@ku.edu>
2019-09-10 12:16:32 -0500

    Implemented search function

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,9 +29,6 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
 	return(m_size);
 }
 
@@ -40,11 +37,17 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
-
-	/** TODO 
-		Fix this method
-	*/
-
+	while((!isFound)&&(temp!=nullptr))
+	{
+		if(temp->getValue()==value)
+		{
+			isFound = true;
+		}
+		else
+		{
+			temp = temp->getNext();
+		}
+	}
 	return(isFound);
 }
 
